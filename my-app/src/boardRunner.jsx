f2ed33e701a5ce40caf2f61e23866c9dd920d0aa
bradym437 <bradymitchell85@gmail.com>
2025-04-26 16:15:53 -0700

    testing my files

---
+++ my-app/src/boardRunner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BingoGame from "./BingoGame"; // Adjust the path if needed
+
+const App = () => {
+    return (
+        <div>
+            <BingoGame />
+        </div>
+    );
+};
+
+export default App;
