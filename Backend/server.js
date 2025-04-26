1867c203789e5157f2b0f2cef8a209d605b1f9b0
Isaac Nelson <ihmnelson@outlook.com>
2025-04-25 22:46:25 -0700

    new stuff

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,5 @@
+const helperFunctions = require('./functions/myFunctions');
+console.log(add(2, 3)); // 5
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -15,12 +17,28 @@ app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
-app.post('/api/signup', (req,res) => {
-  // needs to be implemented (NEW USER)
+app.get('/users/:id', (req, res) => {
+  // TODO: show specific user
+});
+
+app.post('/users/signup/:id', (req,res) => {
+  // TODO: SIGNUP
 }); 
 
-app.patch('/api/game', (req, res) => {
-  // needs to be implemented (USER CHECKS OR UNCHECKS BOX, maybe saves? anyways EXISTING USER PLAYS)
+app.post('/users/login/:id', (req, res) => {
+  // TODO: LOGIN
+});
+
+app.patch('/users/changepassword/:id', (req, res) => {
+  // TODO: update user file (new password)
+});
+
+app.patch('/users/changeusername/:id', (req, res) => {
+  // TODO: update user file (new username)
+});
+
+app.patch('/game/update/:id', (req, res) => {
+  // TODO: update user file (gameplay)
 });
 
 app.get('/api/game', (req, res) => {
